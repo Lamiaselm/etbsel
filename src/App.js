d58244ca7253da978ebfc48dfd5ab3ad1d966b0e
lamiasln <lamia_sn@outlook.fr>
2020-01-08 00:46:18 +0100

    version3.0

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import NavBar from './Components/NavBar'
-
+import {Route, Router} from "react-router";
+import {BrowserRouter} from "react-router-dom";
 import './App.css';
+import Home from './Screens/Home';
 
 function App() {
   return (
-   <NavBar/>
+<BrowserRouter>
+
+<Home/>
+</BrowserRouter>
+   
   )
 }
 
--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import Button from '../Components/Button';
 import NavBar from '../Components/NavBar';
-import logo from '../Assets/logo.png';
-import homePage from '../Assets/homePage.png';
+import logo from '../Assets/LOGO.png';
+import homePage from '../Assets/home_Page.png';
 class Home extends React.Component{
     render (){
         return (
-            <body className="home">
-
-            </body>
+            <div className="home"> 
+            
+            <img className="logo" src={logo}></img>
+             </div>
+           
+          
 
         )
     }
